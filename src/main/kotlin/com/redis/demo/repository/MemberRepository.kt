ed7996aefc90ae194a9352a04f3ab16b64958e93
derek <xerodin85@gmail.com>
2022-06-21 09:52:46 +0900

    refactor : custom member Repository extend delete  - 상속 관계 정리.

--- src/main/kotlin/com/redis/demo/repository/MemberRepository.kt
+++ src/main/kotlin/com/redis/demo/repository/MemberRepository.kt
@@ -5,4 +5,4 @@ import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface MemberRepository : CrudRepository<Member, String>//, MemberCustomRepository
+interface MemberRepository : CrudRepository<Member, String>
